0404d3cc0caf912ad6ffb787ca6a6ed753731fe6
Nick Rucci <ld.tech@outlook.com>
2017-06-02 14:28:06 -0600

    Set default response to be null rather than an empty object

--- index.js
+++ index.js
@@ -35,70 +35,70 @@ module.exports = (opts = {}) => {
 
   return async (ctx, next) => {
     // 100 CONTINUE
-    ctx.res.continue = (res = {}) => {
+    ctx.res.continue = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.CONTINUE
     }
 
     // 200 OK
-    ctx.res.ok = (res = {}) => {
+    ctx.res.ok = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.OK
     }
 
     // 201 CREATED
-    ctx.res.created = (res = {}) => {
+    ctx.res.created = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.CREATED
     }
 
     // 202 ACCEPTED
-    ctx.res.accepted = (res = {}) => {
+    ctx.res.accepted = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.ACCEPTED
     }
 
     // 204 NO CONTENT
-    ctx.res.noContent = (res = {}) => {
+    ctx.res.noContent = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.NO_CONTENT
     }
 
     // 400 BAD REQUEST
-    ctx.res.badRequest = (res = {}) => {
+    ctx.res.badRequest = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.BAD_REQUEST
     }
 
     // 401 UNAUTHORIZED
-    ctx.res.unauthorized = (res = {}) => {
+    ctx.res.unauthorized = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.UNAUTHORIZED
     }
 
     // 403 FORBIDDEN
-    ctx.res.forbidden = (res = {}) => {
+    ctx.res.forbidden = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.FORBIDDEN
     }
 
     // 404 NOT FOUND
-    ctx.res.notFound = (res = {}) => {
+    ctx.res.notFound = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.NOT_FOUND
     }
 
     // 408 REQUEST TIME OUT
-    ctx.res.requestTimeOut = (res = {}) => {
+    ctx.res.requestTimeOut = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.REQUEST_TIME_OUT
@@ -112,35 +112,35 @@ module.exports = (opts = {}) => {
     }
 
     // 500 INTERNAL SERVER ERROR
-    ctx.res.internalServerError = (res = {}) => {
+    ctx.res.internalServerError = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.INTERNAL_SERVER_ERROR
     }
 
     // 501 NOT IMPLEMENTED
-    ctx.res.notImplemented = (res = {}) => {
+    ctx.res.notImplemented = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.NOT_IMPLEMENTED
     }
 
     // 502 BAD GATEWAY
-    ctx.res.badGateway = (res = {}) => {
+    ctx.res.badGateway = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.BAD_GATEWAY
     }
 
     // 503 SERVICE UNAVAILABLE
-    ctx.res.serviceUnavailable = (res = {}) => {
+    ctx.res.serviceUnavailable = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.SERVICE_UNAVAILABLE
     }
 
     // 504 GATEWAY TIME OUT
-    ctx.res.gatewayTimeOut = (res = {}) => {
+    ctx.res.gatewayTimeOut = (res = null) => {
       ctx.type = contentType
       ctx.body = res
       ctx.status = statusCodes.GATEWAY_TIME_OUT
