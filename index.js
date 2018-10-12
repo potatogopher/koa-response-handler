62a76f62c4c6e47ea72e5eb54a6cd67f6ea7ed83
Dmytro Borysovskyi <bbbara10@gmail.com>
2018-10-12 11:23:37 +0300

    eslint index.js --fix

--- index.js
+++ index.js
@@ -11,11 +11,17 @@
  * TODO: add all status codes
  */
 const statusCodes = {
+  // 1xx
   CONTINUE: 100,
+  // 2xx
   OK: 200,
   CREATED: 201,
   ACCEPTED: 202,
   NO_CONTENT: 204,
+  // 3xx
+  MOVED_PERMANENTLY: 301,
+  FOUND: 302,
+  // 4xx
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
   FORBIDDEN: 403,
@@ -27,7 +33,9 @@ const statusCodes = {
   GONE: 410,
   PAYLOAD_TOO_LARGE: 413,
   URI_TOO_LONG: 414,
+  UNSUPPORTED_MEDIA_TYPE: 415,
   IM_A_TEAPOT: 418,
+  // 5xx
   INTERNAL_SERVER_ERROR: 500,
   NOT_IMPLEMENTED: 501,
   BAD_GATEWAY: 502,
@@ -49,11 +57,17 @@ function responseHandler({ contentType = 'application/json' } = {}) {
     };
 
     ctx.response = Object.assign(ctx.response || {}, {
+      // 1xx
       continue: withStatus(statusCodes.CONTINUE),
+      // 2xx
       ok: withStatus(statusCodes.OK),
       created: withStatus(statusCodes.CREATED),
       accepted: withStatus(statusCodes.ACCEPTED),
       noContent: withStatus(statusCodes.NO_CONTENT),
+      // 3xx
+      movedPermanently: withStatus(statusCodes.MOVED_PERMANENTLY),
+      found: withStatus(statusCodes.FOUND),
+      // 4xx
       badRequest: withStatus(statusCodes.BAD_REQUEST),
       unauthorized: withStatus(statusCodes.UNAUTHORIZED),
       forbidden: withStatus(statusCodes.FORBIDDEN),
@@ -65,7 +79,9 @@ function responseHandler({ contentType = 'application/json' } = {}) {
       gone: withStatus(statusCodes.GONE),
       payloadTooLarge: withStatus(statusCodes.PAYLOAD_TOO_LARGE),
       uriTooLong: withStatus(statusCodes.URI_TOO_LONG),
+      unsupportedMediaType: withStatus(statusCodes.UNSUPPORTED_MEDIA_TYPE),
       teapot: withStatus(statusCodes.IM_A_TEAPOT),
+      // 5xx
       internalServerError: withStatus(statusCodes.INTERNAL_SERVER_ERROR),
       notImplemented: withStatus(statusCodes.NOT_IMPLEMENTED),
       badGateway: withStatus(statusCodes.BAD_GATEWAY),
