1a42eb40e48711d7dcf835a2cfbefbf1c0bfe346
Dmytro Borysovskyi <bbbara10@gmail.com>
2018-10-12 18:53:59 +0300

    Add more http codes and reformat code & reduce code lines (#6)
    * Reformat code in index.js
    
    * Add more http codes and reformat code
    
    * eslint index.js --fix
    
    * eslint index.js --fix

--- index.js
+++ index.js
@@ -10,13 +10,18 @@
  *
  * TODO: add all status codes
  */
-
 const statusCodes = {
+  // 1xx
   CONTINUE: 100,
+  // 2xx
   OK: 200,
   CREATED: 201,
   ACCEPTED: 202,
   NO_CONTENT: 204,
+  // 3xx
+  MOVED_PERMANENTLY: 301,
+  FOUND: 302,
+  // 4xx
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
   FORBIDDEN: 403,
@@ -28,172 +33,64 @@ const statusCodes = {
   GONE: 410,
   PAYLOAD_TOO_LARGE: 413,
   URI_TOO_LONG: 414,
+  UNSUPPORTED_MEDIA_TYPE: 415,
   IM_A_TEAPOT: 418,
+  // 5xx
   INTERNAL_SERVER_ERROR: 500,
   NOT_IMPLEMENTED: 501,
   BAD_GATEWAY: 502,
   SERVICE_UNAVAILABLE: 503,
   GATEWAY_TIME_OUT: 504,
-}
+};
 
-module.exports = (opts = {}) => {
-  const { contentType = 'text/plain' } = opts
-
+/**
+ * @param {Object} [options={}] - Optional configuration.
+ * @param {Object} [options.contentType=application/json] - Response content-type.
+ * @return {function} Koa middleware.
+ */
+function responseHandler({ contentType = 'application/json' } = {}) {
   return async (ctx, next) => {
-    // 100 CONTINUE
-    ctx.response.continue = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.CONTINUE
-    }
+    const withStatus = status => (res = null) => {
+      ctx.type = contentType;
+      ctx.body = res;
+      ctx.status = status;
+    };
 
-    // 200 OK
-    ctx.response.ok = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.OK
-    }
+    ctx.response = Object.assign(ctx.response || {}, {
+      // 1xx
+      continue: withStatus(statusCodes.CONTINUE),
+      // 2xx
+      ok: withStatus(statusCodes.OK),
+      created: withStatus(statusCodes.CREATED),
+      accepted: withStatus(statusCodes.ACCEPTED),
+      noContent: withStatus(statusCodes.NO_CONTENT),
+      // 3xx
+      movedPermanently: withStatus(statusCodes.MOVED_PERMANENTLY),
+      found: withStatus(statusCodes.FOUND),
+      // 4xx
+      badRequest: withStatus(statusCodes.BAD_REQUEST),
+      unauthorized: withStatus(statusCodes.UNAUTHORIZED),
+      forbidden: withStatus(statusCodes.FORBIDDEN),
+      notFound: withStatus(statusCodes.NOT_FOUND),
+      methodNotAllowed: withStatus(statusCodes.METHOD_NOT_ALLOWED),
+      notAcceptable: withStatus(statusCodes.NOT_ACCEPTABLE),
+      requestTimeOut: withStatus(statusCodes.REQUEST_TIME_OUT),
+      conflict: withStatus(statusCodes.CONFLICT),
+      gone: withStatus(statusCodes.GONE),
+      payloadTooLarge: withStatus(statusCodes.PAYLOAD_TOO_LARGE),
+      uriTooLong: withStatus(statusCodes.URI_TOO_LONG),
+      unsupportedMediaType: withStatus(statusCodes.UNSUPPORTED_MEDIA_TYPE),
+      teapot: withStatus(statusCodes.IM_A_TEAPOT),
+      // 5xx
+      internalServerError: withStatus(statusCodes.INTERNAL_SERVER_ERROR),
+      notImplemented: withStatus(statusCodes.NOT_IMPLEMENTED),
+      badGateway: withStatus(statusCodes.BAD_GATEWAY),
+      serviceUnavailable: withStatus(statusCodes.SERVICE_UNAVAILABLE),
+      gatewayTimeOut: withStatus(statusCodes.GATEWAY_TIME_OUT),
+    });
 
-    // 201 CREATED
-    ctx.response.created = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.CREATED
-    }
-
-    // 202 ACCEPTED
-    ctx.response.accepted = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.ACCEPTED
-    }
-
-    // 204 NO CONTENT
-    ctx.response.noContent = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.NO_CONTENT
-    }
-
-    // 400 BAD REQUEST
-    ctx.response.badRequest = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.BAD_REQUEST
-    }
-
-    // 401 UNAUTHORIZED
-    ctx.response.unauthorized = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.UNAUTHORIZED
-    }
-
-    // 403 FORBIDDEN
-    ctx.response.forbidden = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.FORBIDDEN
-    }
-
-    // 404 NOT FOUND
-    ctx.response.notFound = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.NOT_FOUND
-    }
-
-    // 405 METHOD NOT ALLOWED
-    ctx.response.methodNotAllowed = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.METHOD_NOT_ALLOWED
-    }
-
-    // 406 NOT ACCEPTABLE
-    ctx.response.notAcceptable = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.NOT_ACCEPTABLE
-    }
-
-    // 408 REQUEST TIME OUT
-    ctx.response.requestTimeOut = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.REQUEST_TIME_OUT
-    }
-
-    // 409 CONFLICT
-    ctx.response.conflict = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.CONFLICT
-    }
-
-    // 410 GONE
-    ctx.response.gone = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.GONE
-    }
-
-    // 413 PAYLOAD TOO LARGE
-    ctx.response.payloadTooLarge = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.PAYLOAD_TOO_LARGE
-    }
-
-    // 414 URI TOO LONG
-    ctx.response.uriTooLong = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.URI_TOO_LONG
-    }
-
-    // 418 I'M A TEAPOT
-    ctx.response.teapot = (res = 'I\'m a teapot') => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.IM_A_TEAPOT
-    }
-
-    // 500 INTERNAL SERVER ERROR
-    ctx.response.internalServerError = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.INTERNAL_SERVER_ERROR
-    }
-
-    // 501 NOT IMPLEMENTED
-    ctx.response.notImplemented = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.NOT_IMPLEMENTED
-    }
-
-    // 502 BAD GATEWAY
-    ctx.response.badGateway = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.BAD_GATEWAY
-    }
-
-    // 503 SERVICE UNAVAILABLE
-    ctx.response.serviceUnavailable = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.SERVICE_UNAVAILABLE
-    }
-
-    // 504 GATEWAY TIME OUT
-    ctx.response.gatewayTimeOut = (res = null) => {
-      ctx.type = contentType
-      ctx.body = res
-      ctx.status = statusCodes.GATEWAY_TIME_OUT
-    }
-
-    return next()
-  }
+    return next();
+  };
 }
+
+module.exports = responseHandler;
